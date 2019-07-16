e68377210ece6a03399a9423477a317e96b1e668
Louis-Marie GENNETAY <louismarie.gennetay@viacesi.fr>
2019-07-16 13:56:51 +0200

    Base du HTML

---
+++ Connection/connection.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Fichier de connexion à la base de données
+ * @author : Louis-Marie GENNETAY
+ */
+
+ $hostMSQL = "localhost";
+ $databaseMSQL = "";
+ $loginMSQL = "";
+ $passwordMSQL = "";
+ 
