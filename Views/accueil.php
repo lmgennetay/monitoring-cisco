0f219d03868c759aec978c823a5687b2938800aa
Louis-Marie GENNETAY <louismarie.gennetay@viacesi.fr>
2019-07-16 14:06:53 +0200

    Création de la vue accueil

---
+++ Views/accueil.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="description" content="Monitoring CISCO">
+        <meta name="keywords" content="HTML, CSS, PHP">
+        <meta name="author" content="Julien DROUART, Louis-Marie GENNETAY, Louis MAUGER">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Monitoring CISCO</title>
+        <link rel="stylesheet" href="Assets/Css/style.css">
+        <link rel="icon" href="Assets/Images/favicon.png">
+    </head>
+
+    <body>
+        <header class="menu"><?php include_once('Views/menu.php') ?></header>
+        <footer><?php include_once('Views/footer.php') ?></footer>
+    </body>
+</html>
