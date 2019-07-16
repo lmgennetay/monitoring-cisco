328f55fcb0ef60963fc2618cc2eb19d14e4f90a6
Louis-Marie GENNETAY <louismarie.gennetay@viacesi.fr>
2019-07-16 16:47:08 +0200

    design

--- Views/menu.php
+++ Views/menu.php
@@ -1,4 +1,7 @@
 <nav>
+    <div class="logo-container">
+        <img src="Assets/Images/logo.png" alt="Cisco" class="logo">
+    </div>
     <h1 class="blue">Monitoring CISCO</h1>
     <ul>
         <li><a href="/">Liste des appareils</a></li>
